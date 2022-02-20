9032beae8782e0f2ad0ec06157f932addcd63399
YOONJINWOO <jiwyoon1@naver.com>
2022-02-20 23:56:34 +0900

    22_02_20_EnemyRogueZombie_Dev

---
+++ EnemyRogueZombie.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyRogue.h"
+#include "EnemyRogueZombie.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASTLE_IN_DUNGEON_API AEnemyRogueZombie : public AEnemyRogue
+{
+	GENERATED_BODY()
+
+public:
+	virtual void EnemyWeaponInit();
+	virtual void EnemyRogueBodyInit();
+	virtual void EnemyRogueCollisionInit();
+	virtual void EnemyRogueStateInit();
+	void EnemyRogueTakeDamegeDelegateInit();
+	virtual void TakeStackInit();
+	virtual void AllStateInit();
+	virtual void AllDotTickInit();
+	virtual void StateEffectInit();
+
+	virtual void EnemyRogueBodySetting();
+	virtual void EnemyRogueStateSetting();
+};
